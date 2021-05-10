138b20b6097733ce2be7f5ec84163bb03d412b22
Ján Kľuka <jan.kluka@fmph.uniba.sk>
2021-05-10 16:19:20 +0200

    Updates

---
+++ precache-manifest.130fe5f63c4e2086a7fc6173bb0b0767.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02430a1f9d4df113715c52a01281a6af",
+    "url": "/resolution-editor/index.html"
+  },
+  {
+    "revision": "ad82759624d29868ee74",
+    "url": "/resolution-editor/static/js/2.acd7f87a.chunk.js"
+  },
+  {
+    "revision": "6d6c0d1371e570614ef23e12cbfed171",
+    "url": "/resolution-editor/static/js/2.acd7f87a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "489d4aba8294d107ff65",
+    "url": "/resolution-editor/static/js/main.c97a2d49.chunk.js"
+  },
+  {
+    "revision": "f28bc1d2ed347baad375",
+    "url": "/resolution-editor/static/js/runtime-main.0ef3d098.js"
+  }
+]);
